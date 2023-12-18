2dcdbc9a951a666079118cea18b8ee8627418b99
Joao Ribeiro <joaolucas.jlsr@gmail.com>
2023-12-17 22:29:31 -0300

    first commit

---
+++ Cevem.Caixa.Web/Services/DatabaseManagementService.cs
@@ -0,0 +1,17 @@
+﻿using Cevem.Caixa.Web.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Cevem.Caixa.Web.Services
+{
+    public static class DatabaseManagementService
+    {
+        public static void MigrationInitialisation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<ApplicationDbContext>()!.Database.Migrate();
+            }
+        }
+    }
+}
